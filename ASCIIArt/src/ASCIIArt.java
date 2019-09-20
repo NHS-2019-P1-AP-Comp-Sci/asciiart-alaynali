7132a72ed10fb2eab94e44fa2a6debe38155dd9b
s-liala <s-liala@53S500042541.it.bsd405.org>
2019-09-20 08:08:31 -0700

    Turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -14,14 +14,102 @@
  *
  */
 
-// YOU CODE HERE :) 
+
+/**
+ * output:
+ * 
+ * 
+ * VICTOR RODRIGUEZ IS BACK FROM THE DEAD
+ *     ____     ____
+ *    /    \___/    \
+ *   /               \ 
+ *  /  | RODRIGUEZ |  \
+ *  |__|    ___    |__|
+ *     |   / | \   |
+ *     |   \_|_/   |
+ *     |   / | \   |
+ *     |   \_|_/   |
+ *     |___________|
+ *   
+ *   
+ */  
 
 public class ASCIIArt {
+	// goes to the next line
+	public static void newLine() {
+		System.out.println();
+	}
+	
+	// upper neck/shoulder region
+	public static void shoulderTop() {
+		System.out.print("     ____");
+	}
+	public static void shoulderEnd() {
+		System.out.print("/    \\");
+	}
+	public static void shoulder() {
+		shoulderTop();
+		shoulderTop();
+		newLine();
+		System.out.print("    ");
+		shoulderEnd();
+		System.out.print("___");
+		shoulderEnd();
+		newLine();
+		System.out.print("   /\t\t   \\");
+		newLine();
+	}
+	
+	// sleeves/name
+	public static void sleeveEnd() {
+		System.out.print("|__|");
+	}
+	public static void name() {
+		System.out.print("/  | RODRIGUEZ |  \\");
+	}
+	public static void sleeve() {
+		System.out.print("  ");
+		name();
+		newLine();
+		System.out.print("  ");
+		sleeveEnd();
+		System.out.print("    ___    ");
+		sleeveEnd();
+		newLine();
+	}
+	
+	// back/number
+	public static void shirtEdge() {
+		System.out.print("   |   ");
+	}
+	public static void numberHalf() {
+		System.out.print("  ");
+		shirtEdge();
+		System.out.print("/ | \\");
+		shirtEdge();
+		newLine();
+		
+		System.out.print("  ");
+		shirtEdge();
+		System.out.print("\\_|_/");
+		shirtEdge();
+		newLine();
+	}
+	public static void number() {
+		numberHalf();
+		numberHalf();
+	}
+	
+	// end of shirt
+	public static void shirtBottom() {
+		System.out.print("     |___________|\n");
+	}
+	
 	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+		System.out.println("\n\"VICTOR RODRIGUEZ IS BACK FROM THE DEAD !!\"\n");
+		shoulder();
+		sleeve();
+		number();
+		shirtBottom();
 	}
 }
